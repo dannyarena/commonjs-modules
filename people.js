860011ba6ae3b30deaa906594417a8510cc11f35
dannyarena <arenadanny71@gmail.com>
2025-05-08 20:00:17 +0200

    Implement createPerson function to generate full name and hobbies

--- people.js
+++ people.js
@@ -2,5 +2,6 @@ const createName = require('./names.js');
 const createHobbies = require('./hobbies.js');
 
 function createPerson ( ) {
-    
+    const fullName = createName ('Danny', 'Arena');
+    const hobbies = createHobbies ('calcio', 'musica', 'lettura');
 }
